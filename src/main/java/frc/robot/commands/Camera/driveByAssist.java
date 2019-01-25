4858d272bb0f8065dfe47f4881cb6924a79919d7
Wendy <vageekmom@gmail.com>
2019-01-25 14:36:24 -0500

    Drive by assist mods

--- src/main/java/frc/robot/commands/Camera/driveByAssist.java
+++ src/main/java/frc/robot/commands/Camera/driveByAssist.java
@@ -22,12 +22,13 @@ public class driveByAssist extends PIDCommand {
   public driveByAssist(CAMERA location) {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-    super(0.01, 0.001, 0.001);   // PID values to use on Aiming.
 
-    this.location = location;
+    super(0.01, 0.001, 0.001);   // PID values to use on Aiming.
 
     requires(Drivetrain.getInstance());
     requires(Camera.getInstance());
+
+    this.location = location;
 
     getPIDController().setAbsoluteTolerance(2);
   }
@@ -58,18 +59,19 @@ public class driveByAssist extends PIDCommand {
   protected void usePIDOutput(double output) {
     SmartDashboard.putNumber("PID Output: ", output);
 
-    // Output is the PID out tx -- rotational degrees
+    // Output is the PID controllers output for tx -- rotational degrees
     // Forward is just driver Y stick value.
     
-    double distance_adjust = OI.getInstance().getDriverY();
+    double distance_adjust = OI.getInstance().getDriverY();  // forward throttle
 
-    Drivetrain.getInstance().tankDrive(output - distance_adjust, output + distance_adjust);
+    Drivetrain.getInstance().tankDrive(distance_adjust + output, distance_adjust - output);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    
+    return (Camera.getInstance().IsTargetFound() == false);
   }
 
   // Called once after isFinished returns true
